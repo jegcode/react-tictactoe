484fdde45064fb41ca6b4abecd0deff13abf9dd7
Jorge Celorio <jegcelorio@yahoo.com>
2020-02-01 19:37:19 -0600

    Added X or O turns. Square is now function comp.

--- src/index.js
+++ src/index.js
@@ -3,35 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-/* controlled component as class */
-class Square extends React.Component{
 
-    render(){
-        return(
-            <button 
-                className="square" 
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+// Refactored Square component to a function component 
+function Square(props){
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
-
-/* Refactor Square component to a function component */
 
 class Board extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i){
@@ -41,7 +38,7 @@ class Board extends React.Component{
     }
 
     render(){
-        const status = "Next Player : X";
+        const status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O');
 
         return(
             <div>
